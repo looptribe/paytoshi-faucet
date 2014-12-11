d77286e17f20bfbad322b887ee73065e094ce277
Diego Ferri <ferri.diego@gmail.com>
2014-12-11 17:41:16 +0100

    Build script.

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Paytoshi Faucet Script Builder"
+
+echo "Please specify script version:"
+read version
+
+filename=paytoshi-faucet-v$version.zip
+
+zip -r $filename . -x ".git*" cache\* nbproject\* "composer.*" build.sh "paytoshi-faucet-v*"
+
+if [ -f $FILE ];
+then
+    actualsize=$(wc -c "$filename" | cut -f 1 -d ' ')
+	echo "File created successfully ($actualsize bytes)."
+else
+	echo "Unable to produce zip file."
+fi
