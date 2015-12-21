dcefa3f6c598a185d5cf132f128c477181a7a86c
Andrea Ghidini <ghidosoft@gmail.com>
2015-12-21 15:24:10 +0100

    Integration tests for Setup module

--- tests/Integration/SetupTest.php
+++ tests/Integration/SetupTest.php
@@ -18,7 +18,7 @@ class SetupTest extends WebTestCase
     /**
      * @runInSeparateProcess
      */
-    public function testIndex()
+    public function testStart()
     {
         $mock = $this->getMockBuilder('Looptribe\Paytoshi\Model\SettingsRepository')
             ->disableOriginalConstructor()
@@ -36,5 +36,56 @@ class SetupTest extends WebTestCase
         $crawler = $client->followRedirect();
 
         $this->assertGreaterThan(0, $crawler->filter('html:contains("Paytoshi Faucet setup")')->count());
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testCheck()
+    {
+        $setupDiagnostics = $this->getMockBuilder('Looptribe\Paytoshi\Model\SetupDiagnostics')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $setupDiagnostics->expects($this->once())
+            ->method('checkDatabase');
+        $this->app['setup.diagnostics'] = $setupDiagnostics;
+
+        $client = $this->createClient();
+        $content = '{"database":{"dbname":"paytoshi_faucet","user":"username","password":"pass","host":"localhost"}}';
+        $client->request('POST', '/setup/check.json', array(), array(), array('CONTENT_TYPE' => 'application/json'), $content);
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertTrue($client->getResponse()->headers->contains('Content-Type', 'application/json'), 'Reponse should be application/json');
+
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertTrue($response['ok'], 'Response should contains an ok field with a true value');
+        $this->assertFalse($response['errors']['db']);
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testCheckDbFail()
+    {
+        $setupDiagnostics = $this->getMockBuilder('Looptribe\Paytoshi\Model\SetupDiagnostics')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $setupDiagnostics->expects($this->once())
+            ->method('checkDatabase')
+            ->willThrowException(new \RuntimeException('Failed!'));
+        $this->app['setup.diagnostics'] = $setupDiagnostics;
+
+        $client = $this->createClient();
+        $content = '{"database":{"dbname":"paytoshi_faucet","user":"username","password":"pass","host":"localhost"}}';
+        $client->request('POST', '/setup/check.json', array(), array(), array('CONTENT_TYPE' => 'application/json'), $content);
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertTrue($client->getResponse()->headers->contains('Content-Type', 'application/json'), 'Reponse should be application/json');
+
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertFalse($response['ok'], 'Response should contains an ok field with a false value');
+        $this->assertNotEmpty($response['errors']['db']);
     }
 }
