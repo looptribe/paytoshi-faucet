21ad50b9d269bff51f1833ed7954e621114aa39b
Diego Ferri <ferri.diego@gmail.com>
2016-01-24 20:15:48 +0100

    IndexController tests. refs #15.

--- tests/Controller/IndexControllerTest.php
+++ tests/Controller/IndexControllerTest.php
@@ -21,6 +21,7 @@ class IndexControllerTest extends \PHPUnit_Framework_TestCase
         $session = $this->getMock('Symfony\Component\HttpFoundation\Session\SessionInterface');
         $flashbag = $this->getMock('Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface');
         $captchaProvider = $this->getMock('Looptribe\Paytoshi\Captcha\CaptchaProviderInterface');
+        $rewardProvider = $this->getMock('Looptribe\Paytoshi\Logic\RewardProviderInterface');
 
         $request->method('get')
             ->willReturn('');
@@ -31,14 +32,44 @@ class IndexControllerTest extends \PHPUnit_Framework_TestCase
         $session->method('get')
             ->willReturn('');
 
-        $themeProvider->method('getTemplate')
+        $themeProvider->expects($this->once())
+            ->method('getTemplate')
+            ->with('index.html.twig')
             ->willReturn('default/index.html.twig');
 
         $templating->expects($this->once())
             ->method('render')
+            ->with('default/index.html.twig',
+                $this->callback(function($data) {
+                    return array_key_exists('referral', $data) &&
+                        array_key_exists('address', $data) &&
+                        array_key_exists('flashbag', $data) &&
+                        array_key_exists('name', $data) &&
+                        array_key_exists('description', $data) &&
+                        array_key_exists('referral_percentage', $data) &&
+                        array_key_exists('rewards', $data) &&
+                        array_key_exists('rewards_average', $data) &&
+                        array_key_exists('rewards_max', $data) &&
+                        array_key_exists('waiting_interval', $data) &&
+                        array_key_exists('captcha', $data) &&
+                        array_key_exists('provider', $data['captcha']) &&
+                        array_key_exists('public_key', $data['captcha']) &&
+                        array_key_exists('content', $data) &&
+                        array_key_exists('header_box', $data['content']) &&
+                        array_key_exists('left_box', $data['content']) &&
+                        array_key_exists('right_box', $data['content']) &&
+                        array_key_exists('center1_box', $data['content']) &&
+                        array_key_exists('center2_box', $data['content']) &&
+                        array_key_exists('center3_box', $data['content']) &&
+                        array_key_exists('footer_box', $data['content']) &&
+                        array_key_exists('theme', $data) &&
+                        array_key_exists('name', $data['theme']) &&
+                        array_key_exists('css', $data['theme']);
+                }
+            ))
             ->willReturn(new Response());
 
-        $sut = new IndexController($templating, $themeProvider, $settingsRepository, $flashbag, $captchaProvider);
+        $sut = new IndexController($templating, $themeProvider, $settingsRepository, $flashbag, $captchaProvider, $rewardProvider);
         $response = $sut->action($request);
 
         $this->assertInstanceOf('Symfony\Component\HttpFoundation\Response', $response);
