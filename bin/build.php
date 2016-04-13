c8c61af4deba8b9d2073879fac618a270bb24a8c
Andrea Ghidini <ghidosoft@gmail.com>
2016-04-13 17:26:02 +0200

    Build script

---
+++ bin/build.php
@@ -0,0 +1,36 @@
+<?php
+
+echo 'Paytoshi Faucet Script Builder' . PHP_EOL;
+
+$root = realpath(__DIR__ . DIRECTORY_SEPARATOR . '..') . DIRECTORY_SEPARATOR;
+$outPath = $root . 'dist' . DIRECTORY_SEPARATOR;
+
+$composer = file_get_contents("${root}composer.json");
+if ($composer === false) {
+    throw new Exception("Cannot read ${root}composer.json");
+}
+$composerJson = json_decode($composer, true);
+if ($composerJson === false) {
+    throw new Exception("Cannot parse json ${root}composer.json");
+}
+
+$version = $composerJson['version'];
+
+$out = "${outPath}paytoshi-faucet-v${version}.zip";
+
+if (file_exists($out)) {
+    echo "Warning: output file $out already exists." . PHP_EOL;
+    unlink($out);
+}
+
+if (!file_exists($outPath) && mkdir($outPath, 0755, true) === false) {
+    throw new Exception("Cannot create directory $outPath");
+}
+
+echo "Writing out file: $out" . PHP_EOL;
+
+chdir($root);
+exec(sprintf('zip -r %s . -x .git\* .idea\* coverage\* tests\* phpunit.xml.dist bin\* dist\*',
+    escapeshellarg($out)));
+
+echo 'Done.' . PHP_EOL;
